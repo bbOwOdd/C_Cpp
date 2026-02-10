7cba0f0bc19d4adfd7bf37051f6155b83773f595
David Tseng <87758475+bbOwOdd@users.noreply.github.com>
2026-02-10 12:14:02 +0800

    Update test.c

--- Others/test.c
+++ Others/test.c
@@ -342,28 +342,3 @@ void print9x9Table(){
         printf("\n");
     }
 }
-
-// print pascal triangle
-void printPascal(int n){
-    int a[10][10] = {0};
-
-    // 計算帕斯卡三角形
-    for (int i = 0; i < n; i++) {
-        a[i][0] = 1;          // 每一列的第一個都是 1
-        a[i][i] = 1;          // 每一列的最後一個都是 1
-        for (int j = 1; j < i; j++) {
-            a[i][j] = a[i-1][j-1] + a[i-1][j];
-        }
-    }
-
-    // 輸出帕斯卡三角形
-    for (int i = 0; i < n; i++) {
-        for (int space = 0; space < n - i - 1; space++) { // 控制排版（置中）
-            printf(" ");
-        }
-        for (int j = 0; j <= i; j++) {
-            printf("%d ", a[i][j]);
-        }
-        printf("\n");
-    }
-}
